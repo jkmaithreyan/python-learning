a35dbb39b4afa7bd359cc14355498cc99febb64f
MaithreyanJK <maithreyanjk@gmail.com>
2026-08-24 11:28:11 +0530

    added answer

---
+++ number_appear_once.py
@@ -0,0 +1,10 @@
+def num_appear_only_once(numbers):
+    result = []
+
+    for num in numbers:
+        if numbers.count(num) == 1:
+            return num
+
+
+numbers = [4, 1, 2, 1, 2]
+print(num_appear_only_once(numbers))
